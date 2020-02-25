3a8d3bd5d8ade89498581e52637b3c22155c4c22
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-02-25 15:50:00 -0600

    Pulled out tokenizer and improved it slightly Finished tokenizer test

---
+++ MathExpr/Syntax/Tokenizer.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace MathExpr.Syntax
+{
+    internal static class Tokenizer
+    {
+        public enum TokenType
+        {
+            Identifier, Literal, OpenParen, CloseParen,
+            Star, Slash, Plus, Minus, Exponent,
+            Equals, Inequals, Less, Greater, LessEq, GreaterEq,
+            Xor, And, Bang, Percent,
+
+            Error
+        }
+
+        public struct Token
+        {
+            public TokenType Type { get; }
+            public object? Value { get; }
+
+            public int Position { get; }
+            public int Length { get; }
+
+            public double? AsDouble => Value as double?;
+            public string? AsString => Value as string;
+
+            public Token(TokenType type, object? value, int pos, int len)
+            {
+                Type = type; Value = value; Position = pos; Length = len;
+            }
+
+            public override bool Equals(object obj)
+                => obj is Token t && this == t;
+            public static bool operator ==(Token a, Token b)
+                => a.Type == b.Type && Equals(a.Value, b.Value);
+            public static bool operator !=(Token a, Token b)
+                => !(a == b);
+
+            public override string ToString()
+                => $"Token({Type}, {Value?.ToString()})";
+
+            public override int GetHashCode()
+            {
+                var hashCode = 9789246;
+                hashCode = hashCode * -1521134295 + Type.GetHashCode();
+                hashCode = hashCode * -1521134295 + EqualityComparer<object?>.Default.GetHashCode(Value);
+                hashCode = hashCode * -1521134295 + Position.GetHashCode();
+                hashCode = hashCode * -1521134295 + Length.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        private static bool IsIdentifierChar(char c)
+            => (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+        private static bool IsNumberChar(char c)
+            => char.IsNumber(c) || c == '.';
+
+        public static IEnumerable<Token> Tokenize(string text)
+        {
+            int tokenStart;
+            int tokenLen;
+            TokenType currentTokenType;
+
+            Token EmitToken()
+            {
+                var value = currentTokenType switch
+                {
+                    TokenType.Identifier => text.Substring(tokenStart, tokenLen),
+                    TokenType.Literal => (object?)double.Parse(text.Substring(tokenStart, tokenLen), CultureInfo.InvariantCulture),
+                    _ => null
+                };
+                var tok = new Token(currentTokenType, value, tokenStart, tokenLen);
+                return tok;
+            }
+
+            static Token NewToken(TokenType type, ref int i, int len = 1)
+            {
+                var tok = new Token(type, null, i, len);
+                i += len;
+                return tok;
+            }
+
+            for (int i = 0; i < text.Length;)
+            {
+                if (char.IsWhiteSpace(text[i]))
+                    i++;
+                else if (IsIdentifierChar(text[i]))
+                {
+                    currentTokenType = TokenType.Identifier;
+                    tokenStart = i++;
+                    tokenLen = 1;
+                    while (i < text.Length && IsIdentifierChar(text[i]))
+                    {
+                        i++;
+                        tokenLen++;
+                    }
+                    yield return EmitToken();
+                }
+                else if (IsNumberChar(text[i]))
+                {
+                    currentTokenType = TokenType.Literal;
+                    tokenStart = i++;
+                    tokenLen = 1;
+                    while (i < text.Length && IsNumberChar(text[i]))
+                    {
+                        i++;
+                        tokenLen++;
+                    }
+                    yield return EmitToken();
+                }
+                else
+                {
+                    switch (text[i])
+                    {
+                        case '(':
+                            yield return NewToken(TokenType.OpenParen, ref i);
+                            break;
+                        case ')':
+                            yield return NewToken(TokenType.CloseParen, ref i);
+                            break;
+                        case '*':
+                            yield return NewToken(TokenType.Star, ref i);
+                            break;
+                        case '/':
+                            yield return NewToken(TokenType.Slash, ref i);
+                            break;
+                        case '+':
+                            yield return NewToken(TokenType.Plus, ref i);
+                            break;
+                        case '-':
+                            yield return NewToken(TokenType.Minus, ref i);
+                            break;
+                        case '^':
+                            if (text[i + 1] == '^')
+                                yield return NewToken(TokenType.Xor, ref i, 2);
+                            else
+                                yield return NewToken(TokenType.Exponent, ref i);
+                            break;
+                        case '=':
+                            yield return NewToken(TokenType.Equals, ref i);
+                            break;
+                        case '%':
+                            yield return NewToken(TokenType.Percent, ref i);
+                            break;
+                        case '&':
+                            yield return NewToken(TokenType.And, ref i);
+                            break;
+                        case '!':
+                            if (text[i + 1] == '=')
+                                yield return NewToken(TokenType.Inequals, ref i, 2);
+                            else
+                                yield return NewToken(TokenType.Bang, ref i);
+                            break;
+                        case '<':
+                            if (text[i + 1] == '=')
+                                yield return NewToken(TokenType.LessEq, ref i, 2);
+                            else
+                                yield return NewToken(TokenType.Less, ref i);
+                            break;
+                        case '>':
+                            if (text[i + 1] == '=')
+                                yield return NewToken(TokenType.GreaterEq, ref i, 2);
+                            else
+                                yield return NewToken(TokenType.Greater, ref i);
+                            break;
+                        default:
+                            yield return new Token(TokenType.Error, "Unexpected charaget", i++, 1);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
--- MathExprTests/ParserTests.cs
+++ MathExprTests/ParserTests.cs
@@ -13,7 +13,34 @@ namespace MathExprTests
         [Fact]
         public void TokenizeString()
         {
-            var tokens = Tokenizer.Tokenize("a+b*c/d^ehij  % k %( 3.442*ident) ^^ y & y").ToArray();
+            var tokens = Tokenizer.Tokenize("a+b*c/d^ehij  % k %( 3.442*ident) ^^ y & y");
+            var expect = new[]
+            {
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "a", 0, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Plus, null, 1, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "b", 2, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Star, null, 3, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "c", 4, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Slash, null, 5, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "d", 6, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Exponent, null, 7, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "ehij", 8, 4),
+                new Tokenizer.Token(Tokenizer.TokenType.Percent, null, 14, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "k", 16, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Percent, null, 18, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.OpenParen, null, 19, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Literal, 3.442d, 21, 5),
+                new Tokenizer.Token(Tokenizer.TokenType.Star, null, 26, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "ident", 27, 5),
+                new Tokenizer.Token(Tokenizer.TokenType.CloseParen, null, 32, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Xor, null, 34, 2),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "y", 37, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.And, null, 39, 1),
+                new Tokenizer.Token(Tokenizer.TokenType.Identifier, "y", 41, 1),
+            };
+
+            Assert.Equal(expect, tokens);
         }
+
     }
 }
