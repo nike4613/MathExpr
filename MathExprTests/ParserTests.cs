74a7ca664bec817e8dcb562219d6f55a5dc1004d
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-02-24 22:30:58 -0600

    Added tokenizer and test project

---
+++ MathExprTests/ParserTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+using MathExpr.Syntax;
+using System.Linq;
+
+namespace MathExprTests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void TokenizeString()
+        {
+            var tokens = Tokenizer.Tokenize("a+b*c/d^ehij  % k %( 3.442*ident) ^^ y & y").ToArray();
+        }
+    }
+}
