2e4e6d15f40b2ec912dcacab8b834b2118555e9c
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-03-20 03:12:54 -0500

    Documented DataProvidingTransformContext

--- MathExpr/Compiler/DataProvidingTransformContext.cs
+++ MathExpr/Compiler/DataProvidingTransformContext.cs
@@ -5,11 +5,21 @@ using System.Text;
 
 namespace MathExpr.Compiler
 {
+    /// <summary>
+    /// A basic implementation of <see cref="IDataContext"/>, providing a nested dictionary based data storage.
+    /// </summary>
     public abstract class DataProvidingTransformContext : IDataContext
     {
         private readonly DataProvidingTransformContext? parent;
+        /// <summary>
+        /// The data storage dictionary used to store the data in this context.
+        /// </summary>
         protected readonly Dictionary<(Type scope, Type type), object?> DataStore = new Dictionary<(Type, Type), object?>();
 
+        /// <summary>
+        /// Constructs a new context, optionally with a parent context.
+        /// </summary>
+        /// <param name="parent">the parent context</param>
         protected DataProvidingTransformContext(DataProvidingTransformContext? parent = null)
             => this.parent = parent;
 
@@ -18,6 +28,18 @@ namespace MathExpr.Compiler
             public static (Type scope, Type type) Key = (typeof(TScope), typeof(TData));
         }
 
+        /// <summary>
+        /// Attempts to get the data associated with a given type and scope.
+        /// </summary>
+        /// <remarks>
+        /// If the data is not found in this context's data store, the lookup is then forwarded to this
+        /// context's parent, if it has one.
+        /// </remarks>
+        /// <typeparam name="TScope">the scope type that the requested data is associated with</typeparam>
+        /// <typeparam name="TData">the type of the data to retrieve</typeparam>
+        /// <param name="data">the data that was stored, if it existed</param>
+        /// <returns><see langword="true"/> if the data was found either in this context or a parent, 
+        /// <see langword="false"/> otherwise</returns>
         protected bool TryGetData<TScope, TData>([MaybeNullWhen(false)] out TData data)
         {
             if (DataStore.TryGetValue(DataStoreKeyStore<TScope, TData>.Key, out var val))
@@ -37,6 +59,13 @@ namespace MathExpr.Compiler
             }
         }
 
+        /// <summary>
+        /// Adds or sets data in this context, assocaited with a scope.
+        /// </summary>
+        /// <typeparam name="TScope">the scope type that the requested data is associated with</typeparam>
+        /// <typeparam name="TData">the type of the data to retrieve</typeparam>
+        /// <param name="data">the data to store</param>
+        /// <returns><paramref name="data"/></returns>
         protected TData AddData<TScope, TData>(TData data)
         {
             var key = DataStoreKeyStore<TScope, TData>.Key;
@@ -47,6 +76,13 @@ namespace MathExpr.Compiler
             return data;
         }
 
+        /// <summary>
+        /// Gets the data in the given scope, if present, otherwise creating it with <paramref name="creator"/>.
+        /// </summary>
+        /// <typeparam name="TScope">the scope type that the requested data is associated with</typeparam>
+        /// <typeparam name="TData">the type of the data to retrieve</typeparam>
+        /// <param name="creator">a delegate to use to create the data if it does not exist</param>
+        /// <returns>the found or created data</returns>
         public TData GetOrCreateData<TScope, TData>(Func<TData> creator)
         {
             if (TryGetData<TScope, TData>(out var data))
@@ -55,6 +91,12 @@ namespace MathExpr.Compiler
                 return AddData<TScope, TData>(creator());
         }
 
+        /// <summary>
+        /// Sets a data value in a given scope in this context.
+        /// </summary>
+        /// <typeparam name="TScope">the scope type that the requested data is associated with</typeparam>
+        /// <typeparam name="TData">the type of the data to retrieve</typeparam>
+        /// <param name="data">the data to set</param>
         public void SetData<TScope, TData>(TData data)
             => AddData<TScope, TData>(data);
     }
