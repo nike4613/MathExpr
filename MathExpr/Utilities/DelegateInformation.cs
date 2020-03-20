a3faa2d5c6dd16d3781a04055a9cb64a19676d6e
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-03-20 18:36:59 -0500

    Documented DelegateInfo

--- MathExpr/Utilities/DelegateInformation.cs
+++ MathExpr/Utilities/DelegateInformation.cs
@@ -6,10 +6,20 @@ using System.Text;
 
 namespace MathExpr.Utilities
 {
+    /// <summary>
+    /// A container class that contains information about the delegate type <typeparamref name="TDelegate"/>.
+    /// </summary>
+    /// <typeparam name="TDelegate"></typeparam>
     public static class DelegateInformation<TDelegate> 
         where TDelegate : Delegate
     {
+        /// <summary>
+        /// The return type of <typeparamref name="TDelegate"/>.
+        /// </summary>
         public static readonly Type ReturnType;
+        /// <summary>
+        /// The parameter types of <typeparamref name="TDelegate"/>.
+        /// </summary>
         public static readonly Type[] ParamTypes;
 
         static DelegateInformation()
