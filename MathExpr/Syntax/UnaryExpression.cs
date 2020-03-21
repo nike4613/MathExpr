94db78eaca3faf6dff761bfccc0fbdb82d5398c8
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-03-21 02:36:29 -0500

    Documented UnaryExpression

--- MathExpr/Syntax/UnaryExpression.cs
+++ MathExpr/Syntax/UnaryExpression.cs
@@ -1,36 +1,61 @@
 ﻿using MathExpr.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace MathExpr.Syntax
 {
+    /// <summary>
+    /// An expression representing an operation with only one argument.
+    /// </summary>
     public sealed class UnaryExpression : MathExpression
     {
+        /// <summary>
+        /// The type of the <see cref="UnaryExpression"/>.
+        /// </summary>
+        [SuppressMessage("Documentation", "CS1591", Justification = "The names are self-explanatory.")]
         public enum ExpressionType
         {
             Negate, Not, Factorial
         }
 
+        /// <summary>
+        /// The type of this expression.
+        /// </summary>
         public ExpressionType Type { get; }
+        /// <summary>
+        /// The single argument to this expression.
+        /// </summary>
         public MathExpression Argument { get; }
 
+        /// <summary>
+        /// The size of this expression. This is always the size of the argument plus one.
+        /// </summary>
         public override int Size => Argument.Size + 1;
 
+        /// <summary>
+        /// Constructs a new <see cref="UnaryExpression"/> of the specified type, with the given argument.
+        /// </summary>
+        /// <param name="type">the type of the expression</param>
+        /// <param name="arg">the argument of the expression</param>
         public UnaryExpression(ExpressionType type, MathExpression arg)
         {
             Type = type;
             Argument = arg;
         }
 
+        /// <inheritdoc/>
         public override bool Equals(MathExpression other)
             => other is UnaryExpression e 
             && Type == e.Type 
             && Equals(Argument, e.Argument);
 
+        /// <inheritdoc/>
         public override string ToString()
             => $"({Type} {Argument})";
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             var hashCode = -850124847;
