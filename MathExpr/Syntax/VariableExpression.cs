b0af8306c1e9fb66d210b9df2ff9b4dbcf258dc6
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-03-21 02:43:41 -0500

    Documented VariableExpression

--- MathExpr/Syntax/VariableExpression.cs
+++ MathExpr/Syntax/VariableExpression.cs
@@ -4,23 +4,39 @@ using System.Text;
 
 namespace MathExpr.Syntax
 {
+    /// <summary>
+    /// An expression representing a variable.
+    /// </summary>
     public sealed class VariableExpression : MathExpression
     {
+        /// <summary>
+        /// The name of the variable.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// The size of the expression. 
+        /// </summary>
         public override int Size => 1; // because it is one operation to load
 
+        /// <summary>
+        /// Creates a new <see cref="VariableExpression"/> with the specified variable name.
+        /// </summary>
+        /// <param name="name">the name of the variable</param>
         public VariableExpression(string name)
         {
             Name = name;
         }
 
+        /// <inheritdoc/>
         public override bool Equals(MathExpression other)
             => other is VariableExpression v && v.Name == Name;
 
+        /// <inheritdoc/>
         public override string ToString()
             => $"'{Name}'";
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             var hashCode = 890389916;
